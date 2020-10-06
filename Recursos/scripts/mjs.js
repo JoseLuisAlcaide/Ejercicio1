abf3c7c02aad7257c918f442d3725ebeb4e71171
infesid1 <infesid1@iesrodolfoucha.es>
2020-10-06 18:03:44 +0200

    commin2

--- Recursos/scripts/mjs.js
+++ Recursos/scripts/mjs.js
@@ -1,2 +1,14 @@
-let x=prompt("introduce");
-console.log("el numero es:"+x);
+function Mensaje(dia) {
+    alert("Hoy es un "+dia+" dia");
+}
+
+function calcular(){
+let x=5.3;
+
+console.log("El valor de x es "+x);
+console.log(`El valor de x: ${x}`);
+console.log(`El valor de x: ${x} es entero? `+Number.isInteger(x));
+console.log(`La parte entera de ${x} es `+Number.parseInt(x));
+
+y="34"
+}
